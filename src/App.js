265c22ee7ddd35de4638d8ac74f46f08f4971fa8
Zain Sheikh <70570850+ZainJahangir01@users.noreply.github.com>
2024-02-28 23:24:27 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './views/Home';
+import Login from './views/Login';
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'
+// import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import Signup from './views/signup/index.jsx';
+import { CartProvider } from './components/ContextReducer.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </CartProvider>
   );
 }
 
